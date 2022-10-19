f5b558ffbd1798f1d92144b79225f809707645c1
garimakwr <116144056+garimakwr@users.noreply.github.com>
2022-10-19 20:56:58 +0530

    Create lp.js

---
+++ lp.js
@@ -0,0 +1,25 @@
+lpTag.sdes = lpTag.sdes || [];
+lpTag.identities = [];
+function identityFn(callback) {
+    callback({
+        iss: "Garry",
+        acr: "loa1",
+        sub: "4255551212"
+    });
+}
+
+
+lpTag.sdes.push({
+    "type": "cart",
+    "total": 1701,
+    "currency": "USD",
+    "numItems": "1",
+    "products": [{
+        "product": {
+            "name": "Title : Shop Online",
+            "category": " ",
+            "price": " "
+        }
+    }]
+})
+lpTag.identities.push(identityFn);
